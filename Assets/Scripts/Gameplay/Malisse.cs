a8777baa34ada7a33c052a85533327743c1520d2
Renaud Bédard <renaud.bedard@gmail.com>
2014-04-26 18:42:25 -0400

    New stuffs

--- Assets/Scripts/Gameplay/Malisse.cs
+++ Assets/Scripts/Gameplay/Malisse.cs
@@ -23,26 +23,26 @@ class Malisse : MonoBehaviour
         // mostly right, some front
         if (d.x > 0 && Mathf.Abs(d.y) < 0.5 && d.y < 0 && sprite.clipId != sprite.GetClipIdByName("walk_rf"))
         {
-            Debug.Log("rf");
+//            Debug.Log("rf");
             sprite.Play("walk_rf");
         }
         // mostly left, some front
         if (d.x < 0 && Mathf.Abs(d.y) < 0.5 && d.y < 0 && sprite.clipId != sprite.GetClipIdByName("walk_lf"))
         {
-            Debug.Log("lf");
+//            Debug.Log("lf");
             sprite.Play("walk_lf");
         }
 
         // mostly back, some right
         if (d.y > 0.25 && Mathf.Abs(d.x) < 0.5 && d.x > 0 && sprite.clipId != sprite.GetClipIdByName("walk_rb"))
         {
-            Debug.Log("rb");
+//            Debug.Log("rb");
             sprite.Play("walk_rb");
         }
         // mostly back, some left
         if (d.y > 0.25 && Mathf.Abs(d.x) < 0.5 && d.x < 0 && sprite.clipId != sprite.GetClipIdByName("walk_lb"))
         {
-            Debug.Log("lb");
+//            Debug.Log("lb");
             sprite.Play("walk_lb");
         }
     }
---
+++ Assets/Scripts/Gameplay/ResizableShadow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+class ResizableShadow : MonoBehaviour
+{
+    Collider parentCollider;
+
+    void Awake()
+    {
+        parentCollider = transform.parent.collider;
+    }
+
+    void Update()
+    {
+        if (parentCollider is BoxCollider)
+        {
+            var length = (parentCollider as BoxCollider).size.x;
+            var depth = (parentCollider as BoxCollider).size.z;
+            transform.localScale = new Vector3(length, depth, 1);
+        }
+        else if (parentCollider is MeshCollider)
+        {
+            var length = (parentCollider as MeshCollider).bounds.size.x;
+            var depth = (parentCollider as MeshCollider).bounds.size.z;
+
+            transform.localScale = new Vector3(length / transform.parent.localScale.x / transform.parent.parent.localScale.x, 
+                                               depth / transform.parent.localScale.y / transform.parent.parent.localScale.y, 
+                                               1);
+            
+        }
+
+        transform.localPosition = Vector3.zero;
+        transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+
+        var height = Mathf.Max(transform.parent.position.y, 1);
+        transform.localScale *= Mathf.Min(100.0f / height, 1);
+
+        if (transform.parent.GetComponent<tk2dAnimatedSprite>() == null)
+            transform.rotation = Quaternion.Euler(90, 0, 0);
+        else
+            transform.rotation = Quaternion.identity;
+    }
+}
