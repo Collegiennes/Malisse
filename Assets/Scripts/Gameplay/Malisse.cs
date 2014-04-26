bda7c5e41d4be3c52abd0a2d64c8d58632f6b396
Renaud Bédard <renaud.bedard@gmail.com>
2014-04-26 12:44:47 -0400

    Fixed malisse lookat

--- Assets/Scripts/Gameplay/Malisse.cs
+++ Assets/Scripts/Gameplay/Malisse.cs
@@ -2,9 +2,16 @@
 
 class Malisse : MonoBehaviour
 {
+    Camera mainCamera;
+
     void Awake()
     {
-        if (Camera.main)
-            transform.rotation = Camera.main.transform.rotation;
+        mainCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
+        transform.rotation = mainCamera.transform.rotation;
+    }
+
+    void Update()
+    {
+        //transform.LookAt(mainCamera.transform.position);
     }
 }
