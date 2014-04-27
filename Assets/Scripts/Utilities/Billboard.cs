36dfaec7e9e2f13e4ef77a10fa04e3cf55674df2
Renaud Bédard <renaud.bedard@gmail.com>
2014-04-27 10:46:23 -0400

    Billboarding script

---
+++ Assets/Scripts/Utilities/Billboard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class Billboard : MonoBehaviour
+{
+    void Awake()
+    {
+        var camGO = GameObject.Find("MainCamera");
+        if (camGO)
+            transform.rotation = camGO.transform.rotation;
+    }
+}
