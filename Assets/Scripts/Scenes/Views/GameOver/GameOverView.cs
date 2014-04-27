d093b08a72a4b27c1e72629929c895326a48e8c4
technobeanie <plpoirier@technobeanie.com>
2014-04-27 15:55:04 -0400

    Added a GameOver screen.

---
+++ Assets/Scripts/Scenes/Views/GameOver/GameOverView.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameOverView : AlisseView 
+{
+	#region Members and properties
+	// constants
+	
+	// enums
+	
+	// public
+	public float m_MinDelay = 0.5f;
+	
+	// protected
+	
+	// private
+	private float m_CurrentCountdown = 0.0f;
+	
+	// protected
+	
+	// private
+	
+	// properties
+	#endregion
+	
+	#region Unity API
+	protected override void Update()
+	{
+		base.Update();
+		
+		if (m_State != eState.OPENED)
+		{
+			return;
+		}
+
+		if (m_CurrentCountdown > m_MinDelay)
+		{
+			if (ControllerInputManager.Instance.GetButtonDown(ControllerInputManager.eButtonAliases.GRAB.ToString()).Count > 0 || 
+			    ControllerInputManager.Instance.GetButtonDown(ControllerInputManager.eButtonAliases.START.ToString()).Count > 0)
+			{
+				FlowManager.Instance.TriggerAction("GO_TO_MAIN_MENU");
+			}
+		}
+		else
+		{
+			m_CurrentCountdown += Time.deltaTime;
+		}
+	}
+	#endregion
+
+	#region View Implementation
+	#endregion
+	
+	#region Public Methods
+	#endregion
+	
+	#region Protected Methods
+	#endregion
+	
+	#region Private Methods
+	#endregion
+}
