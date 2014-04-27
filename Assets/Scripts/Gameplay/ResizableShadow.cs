da1fe09d2ed5be4982358aa714c43cf4c7fccab5
Renaud Bédard <renaud.bedard@gmail.com>
2014-04-26 20:50:16 -0400

    Shadow tweakz

--- Assets/Scripts/Gameplay/ResizableShadow.cs
+++ Assets/Scripts/Gameplay/ResizableShadow.cs
@@ -4,13 +4,12 @@ class ResizableShadow : MonoBehaviour
 {
     Collider parentCollider;
 
+    Vector3 baseScale;
+
     void Awake()
     {
         parentCollider = transform.parent.collider;
-    }
 
-    void Update()
-    {
         if (parentCollider is BoxCollider)
         {
             var length = (parentCollider as BoxCollider).size.x;
@@ -22,17 +21,24 @@ class ResizableShadow : MonoBehaviour
             var length = (parentCollider as MeshCollider).bounds.size.x;
             var depth = (parentCollider as MeshCollider).bounds.size.z;
 
-            transform.localScale = new Vector3(length / transform.parent.localScale.x / transform.parent.parent.localScale.x, 
-                                               depth / transform.parent.localScale.y / transform.parent.parent.localScale.y, 
+            transform.localScale = new Vector3(length / transform.parent.localScale.x / transform.parent.parent.localScale.x,
+                                               depth / transform.parent.localScale.y / transform.parent.parent.localScale.y,
                                                1);
-            
         }
 
+        baseScale = transform.localScale;
+    }
+
+    void Update()
+    {
         transform.localPosition = Vector3.zero;
         transform.position = new Vector3(transform.position.x, 0, transform.position.z);
 
         var height = Mathf.Max(transform.parent.position.y, 1);
-        transform.localScale *= Mathf.Min(100.0f / height, 1);
+        float distance = Mathf.Clamp01(1 - height / 475.0f);
+        transform.localScale = baseScale * distance;
+
+        renderer.material.color = new Color(1, 1, 1, Mathf.Pow(distance, 1.125f));
 
         if (transform.parent.GetComponent<tk2dAnimatedSprite>() == null)
             transform.rotation = Quaternion.Euler(90, 0, 0);
